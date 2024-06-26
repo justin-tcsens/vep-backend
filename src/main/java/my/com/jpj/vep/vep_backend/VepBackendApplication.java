1a4b086c4e2c1e2ba0517d48a61e5bdc02fea546
justin.phua <justin.phua@tcsens.com>
2024-06-26 08:37:23 +0800

    Initial commit

---
+++ src/main/java/my/com/jpj/vep/vep_backend/VepBackendApplication.java
@@ -0,0 +1,13 @@
+package my.com.jpj.vep.vep_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VepBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VepBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/com/jpj/vep/vep_backend/VepBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package my.com.jpj.vep.vep_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VepBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
